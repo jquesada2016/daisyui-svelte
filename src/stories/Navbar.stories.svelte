19a40d0d29daae7f225f653c72ef98bdb44a2e21
Jose Quesada <jquesada2016@fau.edu>
2021-12-20 12:07:11 -0600

    initial commit

---
+++ src/stories/Navbar.stories.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { Navbar } from "../lib/components";
+</script>
+
+<Meta title="Navbar" component={Navbar} />
+
+<Story name="Navbar" />
