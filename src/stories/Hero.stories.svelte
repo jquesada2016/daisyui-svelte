fe6c7bac5ea9c800626263ac003b7bdb867689bb
Jose Quesada <jquesada2016@fau.edu>
2021-12-20 13:04:49 -0600

    removed some components that might be too opinionated

--- src/stories/Hero.stories.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import { Hero } from "../lib/components";
-</script>
-
-<Meta title="Hero" component={Hero} />
-
-<Story name="Hero" />
